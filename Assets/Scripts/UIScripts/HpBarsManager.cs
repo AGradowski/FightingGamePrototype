5256f92c2168ec5a2adf87867618a8abaa9aace8
Antoni Gradowski <antoni.gradowski@gmail.com>
2026-04-01 21:16:37 +0200

    Added the filling bar, and usage of the name file

--- Assets/Scripts/UIScripts/HpBarsManager.cs
+++ Assets/Scripts/UIScripts/HpBarsManager.cs
@@ -3,41 +3,39 @@ using UnityEngine.UIElements;
 
 public class HpBarsManager : MonoBehaviour
 {
+    public FightManager manager;
 
-    public FightManager manager;//possibly add teh search function
-    //public Player player1;
-    //public Player player2;
     public UIDocument UIDoc;
-    private Label m_HealthLabelP1;
-    private Label m_HealthLabelP2;
+    private Label ui_HealthLabelP1;
+    private VisualElement ui_HealtBarMaskP1;
+    private Label ui_HealthLabelP2;
+    private VisualElement ui_HealtBarMaskP2;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //below are the queries searching the UI doc for all teh required elements
+        ui_HealthLabelP1 = UIDoc.rootVisualElement.Q<Label>(UINames.LABEL_OF_PLAYER1);
+        ui_HealtBarMaskP1 = UIDoc.rootVisualElement.Q<VisualElement>(UINames.HP_MASK_PLAYER1);
+
+        ui_HealthLabelP2 = UIDoc.rootVisualElement.Q<Label>(UINames.LABEL_OF_PLAYER2);
+        ui_HealtBarMaskP2 = UIDoc.rootVisualElement.Q<VisualElement>(UINames.HP_MASK_PLAYER2);
         Actions.HealthChanged += UpdateHealth;
-
-        m_HealthLabelP1 = UIDoc.rootVisualElement.Q<Label>("HealthLabel1");
-        m_HealthLabelP2 = UIDoc.rootVisualElement.Q<Label>("HealthLabel2");
-
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void UpdateHealth(Player playerHurt)
     {
+        float healthRatio = (float)playerHurt.playerHealthManager.healthValue / playerHurt.playerHealthManager.maxHealthValue;
+        float healthPercent = Mathf.Lerp(0, 100, healthRatio);//Sets the float to range 0-100 (%). This is is easy option
+        //some other scenearios of graphics might require it more
         if (playerHurt.name == Names.PLAYER1)
         {
-            m_HealthLabelP1.text = $"{playerHurt.playerHealthManager.healthValue}/{playerHurt.playerHealthManager.maxHealthValue}";
+            ui_HealthLabelP1.text = $"{playerHurt.playerHealthManager.healthValue}/{playerHurt.playerHealthManager.maxHealthValue}";
+            ui_HealtBarMaskP1.style.width = Length.Percent(healthPercent);
         }
         else if (playerHurt.name == Names.PLAYER2)
         {
-            m_HealthLabelP2.text = $"{playerHurt.playerHealthManager.healthValue}/{playerHurt.playerHealthManager.maxHealthValue}";
+            ui_HealthLabelP2.text = $"{playerHurt.playerHealthManager.healthValue}/{playerHurt.playerHealthManager.maxHealthValue}";
+            ui_HealtBarMaskP2.style.width = Length.Percent(healthPercent);
         }
     }
 }
